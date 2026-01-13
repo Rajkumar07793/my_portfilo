d9f37632be0412cb9ec9614b785a8fa8372c1c78
Rajkumar07793 <rajkumar07793@gmail.com>
2026-01-13 13:56:39 +0530

    updated web app

--- lib/utils/responsive.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class Responsive {
-  static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 850;
-
-  static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1100;
-}
